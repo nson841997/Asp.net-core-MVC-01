e479a9a4a9444031d03c408be78b8bcbed327b7f
Son <nson841997@gmail.com>
2019-12-09 17:21:45 +0700

    update upLoadFile

---
+++ Controllers/UploadController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MvcMovie.Controllers
+{
+    public class UploadController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        
+        [HttpPost]
+        public IActionResult UploadFiles(List<IFormFile> myfile)
+        {
+            // duyệt từng file lưu thư mục chỉ định
+            foreach (IFormFile f in myfile)
+            {
+                string fullPath = Path.Combine(Directory.GetCurrentDirectory(),
+                    "wwwroot", "Myfiles", f.FileName);
+
+                using (var file = new FileStream(fullPath, FileMode.Create))
+                {
+                    f.CopyTo(file);
+                }
+            }
+
+
+
+            return View("Index");
+        }
+    }
+}
